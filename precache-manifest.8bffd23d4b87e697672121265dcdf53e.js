28feb63faf9b31c8e9901d29ded31fa938f2be13
Standy Merizier <standymerizier@Standys-MBP.fios-router.home>
2018-12-06 05:23:57 -0500

    Updates

---
+++ precache-manifest.8bffd23d4b87e697672121265dcdf53e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e8ba548a569ba9b8bcc",
+    "url": "/noteToSelfReact/static/js/runtime~main.1e8ba548.js"
+  },
+  {
+    "revision": "dd9184767fe7645a3a33",
+    "url": "/noteToSelfReact/static/js/main.dd918476.chunk.js"
+  },
+  {
+    "revision": "8859cf684e7cbe18b6b5",
+    "url": "/noteToSelfReact/static/js/1.8859cf68.chunk.js"
+  },
+  {
+    "revision": "dd9184767fe7645a3a33",
+    "url": "/noteToSelfReact/static/css/main.3b1e0aee.chunk.css"
+  },
+  {
+    "revision": "f1652c79b8464765a132e5a3c01b8949",
+    "url": "/noteToSelfReact/index.html"
+  }
+];
